5fceb8e8f13ea04eac9aaef56a781468d19dc7b7
Marksolution <33540488+Marksolution@users.noreply.github.com>
2019-12-24 22:21:18 +0700

    new

--- app/src/main/java/com/cis/android_layout/ConstranActivity.kt
+++ app/src/main/java/com/cis/android_layout/ConstranActivity.kt
@@ -1,12 +1,20 @@
 package com.cis.android_layout
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_constran.*
 
 class ConstranActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_constran)
+
+        button5.setOnClickListener {
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+        }
     }
+
 }
--- app/src/main/java/com/cis/android_layout/MainActivity.kt
+++ app/src/main/java/com/cis/android_layout/MainActivity.kt
@@ -3,6 +3,7 @@ package com.cis.android_layout
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_constran.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,17 +12,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button.setOnClickListener {
-            val i = Intent(this, LinearActivity::class.java)
-            startActivity(i)
-        }
-        button2.setOnClickListener {
-            val i = Intent(this, RelativeActivity::class.java)
-            startActivity(i)
-        }
+
         button3.setOnClickListener {
-            val i = Intent(this, LinearActivity::class.java)
+            val i = Intent(this, ConstranActivity::class.java)
             startActivity(i)
         }
+
     }
 }
